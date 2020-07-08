6474b436591c6d84dfb588cdc3cb770bf1e5b8ad
Weldameshi <47304615+Weldameshi@users.noreply.github.com>
2020-07-08 10:32:38 -0600

    getting started got reddis figured out

--- eCommerceCart/src/main/java/eCommerceCart/Controller.java
+++ eCommerceCart/src/main/java/eCommerceCart/Controller.java
@@ -1,5 +1,12 @@
 package eCommerceCart;
-
+import redis.clients.jedis.Jedis;
 public class Controller {
-
+	   @SuppressWarnings("resource")
+	public static void main(String[] args) { 
+		      //Connecting to Redis server on localhost 
+		      Jedis jedis = new Jedis("localhost"); 
+		      System.out.println("Connection to server sucessfully"); 
+		      //check whether server is running or not 
+		      System.out.println("Server is running: "+jedis.ping()); 
+		   } 
 }
--- eCommerceCart/src/main/java/eCommerceCart/Item.java
+++ eCommerceCart/src/main/java/eCommerceCart/Item.java
@@ -2,4 +2,38 @@ package eCommerceCart;
 
 public class Item {
 
+	private long iD;
+	
+	private String title;
+	
+	private String description;
+	
+	private double unitPrice;
+	
+	
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public long getiD() {
+		return iD;
+	}
+	public void setiD(long iD) {
+		this.iD = iD;
+	}
+
 }
